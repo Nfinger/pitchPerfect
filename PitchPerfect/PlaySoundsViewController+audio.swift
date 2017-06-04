25e971c82c34ba4762954267453a06bf58a81ee4
Nathaniel Finger <nathanielfinger@Nathaniels-MacBook-Pro-2.local>
2017-06-04 08:10:40 -0400

    first commit

---
+++ PitchPerfect/PlaySoundsViewController+audio.swift
@@ -0,0 +1,9 @@
+//
+//  PlaySoundsViewController+audio.swift
+//  PitchPerfect
+//
+//  Created by Nathaniel Finger on 6/3/17.
+//  Copyright © 2017 Nathaniel Finger. All rights reserved.
+//
+
+import Foundation
---
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Nathaniel Finger on 6/3/17.
+//  Copyright © 2017 Nathaniel Finger. All rights reserved.
+//
+
+import AVFoundation
+
+class RecordSoundsViewController {
+    
+    var A
+}
